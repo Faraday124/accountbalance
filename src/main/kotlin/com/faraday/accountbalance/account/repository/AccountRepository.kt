27542a9d403aa92ef31d7fc589e7b643ce5a4de4
Iwo Paprota <iwo.paprota@gmail.com>
2021-02-23 23:13:20 +0100

    chore: refactor

--- src/main/kotlin/com/faraday/accountbalance/account/repository/AccountRepository.kt
+++ src/main/kotlin/com/faraday/accountbalance/account/repository/AccountRepository.kt
@@ -15,15 +15,16 @@ interface AccountRepository {
 @Component
 class InMemoryAccountRepository : AccountRepository {
     companion object {
+        private val CURRENCY_UNIT_PLN = CurrencyUnit.of("PLN")
         val DB: Map<String, Account> = hashMapOf(
                 "12345" to Account("54321", "0000 0000 0000 0000 0001",
-                        Money.of(CurrencyUnit.of("PLN"), BigDecimal("4000.32"))
+                        Money.of(CURRENCY_UNIT_PLN, BigDecimal("4000.32"))
                 ),
                 "54321" to Account("12345", "0000 0000 0000 0000 0009",
-                        Money.of(CurrencyUnit.of("PLN"), BigDecimal("10.02"))
+                        Money.of(CURRENCY_UNIT_PLN, BigDecimal("10.02"))
                 ),
                 "11111" to Account("11112", "0000 0000 0000 0000 0000",
-                        Money.of(CurrencyUnit.of("PLN"), BigDecimal.ZERO)
+                        Money.of(CURRENCY_UNIT_PLN, BigDecimal.ZERO)
                 ),
         )
     }
